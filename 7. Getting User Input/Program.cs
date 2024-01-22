ad7080bbe4aa77e776921471668da0e5b05be9fb
Dell-User <Dell-User@DESKTOP-H6E2ELM>
2024-01-22 15:22:52 +0200

    Add project files.

---
+++ 7. Getting User Input/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Getting_User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.Write("How old are you? ");
+
+            string age = Console.ReadLine();
+            Console.WriteLine("you are " + age);
+
+            Console.ReadLine();
+        }
+    }
+}
